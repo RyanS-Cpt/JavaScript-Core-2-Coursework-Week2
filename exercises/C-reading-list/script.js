55394a011f73bc54e9d6b9853ca0f10993770f61
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-04 21:34:47 +0200

    start c-reading-list

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,5 +1,24 @@
-function readingList(books) {
-  // Write your code here...
+function readingList(bookList) {
+  for (let item in bookList){
+    console.log(bookList[item]);
+  let content = document.getElementById("content");
+  let pEl = document.createElement("p");
+  let list = document.createElement("ul");
+  let listItm = document.createElement("li");
+  let imgEl = document.createElement("img");
+  content.appendChild(list);
+  list.appendChild(listItm);
+  listItm.appendChild(pEl);
+  listItm.appendChild(imgEl);
+  pEl.textContent = bookList[item].title + " - " + bookList[item].author;
+  if (bookList[item].alreadyRead === true){
+    listItm.style.backgroundColor = "green";
+  }else{
+    listItm.style.backgroundColor = "red";
+  }
+  list.style.display = "flex";
+  listItm.style.flexDirection = "row";
+  }
 }
 
 const books = [
