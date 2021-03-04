70908807f46ed0072c2e365dbd9270577dfb9ac8
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-04 20:39:47 +0200

    complete B-shopping-list

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,12 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfItems) {
+  for (let item of arrayOfItems){
+  let content = document.getElementById("content");
+  let list = document.createElement("ul");
+  let listItm = document.createElement("li");
+  listItm.textContent = item;
+  content.appendChild(list);
+  list.appendChild(listItm);
+  }
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
