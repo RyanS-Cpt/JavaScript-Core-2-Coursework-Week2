60bb0dd63c63b52edfe1051a9a54f82ded90dd92
RyanS-Cpt <ryan.30@icloud.com>
2021-03-05 12:13:58 +0200

    Added img src loop

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -16,6 +16,13 @@ function readingList(bookList) {
   }else{
     listItm.style.backgroundColor = "red";
   }
+  if(bookList[item].author === "Don Norman"){
+    imgEl.src = "https://media.takealot.com/covers_tsins/42140536/42140536-1-pdpxl.jpeg";
+  }else if(bookList[item].author === "Brian Christian"){
+    imgEl.src = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1295465264l/8884400.jpg";
+  }else if(bookList[item].author === "Andrew Hunt"){
+    imgEl.src = "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_pragmatic_programmer.jpg/220px-The_pragmatic_programmer.jpg";
+  }
   list.style.display = "flex";
   listItm.style.flexDirection = "row";
   }
