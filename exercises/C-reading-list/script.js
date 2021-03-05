2841a773b10e5ca4b414e246794943c6ba97e4c1
RyanS-Cpt <ryan.30@icloud.com>
2021-03-05 12:46:01 +0200

    Edit styles

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -12,19 +12,20 @@ function readingList(bookList) {
   listItm.appendChild(imgEl);
   pEl.textContent = bookList[item].title + " - " + bookList[item].author;
   if (bookList[item].alreadyRead === true){
-    listItm.style.backgroundColor = "green";
+    list.style.backgroundColor = "green";
   }else{
-    listItm.style.backgroundColor = "red";
+    list.style.backgroundColor = "red";
   }
   if(bookList[item].author === "Don Norman"){
-    imgEl.src = "https://media.takealot.com/covers_tsins/42140536/42140536-1-pdpxl.jpeg";
+    imgEl.src = "https://covers.vitalbook.com/vbid/9780465072996/width/200";
   }else if(bookList[item].author === "Brian Christian"){
     imgEl.src = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1295465264l/8884400.jpg";
   }else if(bookList[item].author === "Andrew Hunt"){
     imgEl.src = "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_pragmatic_programmer.jpg/220px-The_pragmatic_programmer.jpg";
   }
   list.style.display = "flex";
-  listItm.style.flexDirection = "row";
+  list.style.alignContent = "row";
+  list.style.width = "25%";
   }
 }
 
