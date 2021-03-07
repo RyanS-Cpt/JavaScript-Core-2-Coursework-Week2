72d0d08df9550f8a89a5382d067a33a8c6e1a32f
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-07 18:42:30 +0200

    Complete E-list-of-colours

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,22 @@
-function listOfColours(colours) {
-  // Write your code here...
+function listOfColours(colourArr) {
+    let content = document.getElementById("content");
+    let selectEl = document.createElement("select");
+     content.appendChild(selectEl);
+     let pEl = document.createElement("p");
+     content.appendChild(pEl);
+  for (let colour of colourArr){
+      selectedColour = function (){
+      pEl.textContent = "You have selected: " + colourArr[selectEl.selectedIndex];
+      pEl.style.color = colourArr[selectEl.selectedIndex];
+      }
+    pEl.textContent = "You have selected: " + selectEl.selectedIndex;
+    let optionEl = document.createElement("option");
+    selectEl.appendChild(optionEl);
+    optionEl.innerHTML = colour;
+    console.log(colour);
+    selectEl.addEventListener("click", selectedColour);
+  //  console.log(selectEl.selectedIndex);
+  }
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
