5a953c3ef26af8505c7276b72a0eb2529d305deb
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-10 14:19:08 +0200

    Completed Basic tasks

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -18,6 +18,7 @@ function populateTodoList(todosArr) {
     iTwo.setAttribute("aria-hidden","true");
     iOne.addEventListener("click", ()=>{
       newToDo.style.textDecoration = "line-through";
+      newToDo.completed = true;
     });
     iTwo.addEventListener("click", ()=>{
       list.removeChild(newToDo);
@@ -41,8 +42,17 @@ function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  let newArr = [];
+  let todoInput = document.getElementById("todoInput");
+  let newTask = {"task": todoInput.value,
+  completed: false,};
+  newArr.push(newTask);
+  populateTodoList(newArr);
+  todoInput.value = "";
+  // console.log(todos.length);
 }
 
+// populateTodoList(todos);
 // Advanced challenge: Write a function that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
